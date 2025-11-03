e7f9520c53e20db62bf99e588a0bdb0d430d21c7
MINGYU KIM <coinmagic8085@gmail.com>
2025-11-03 20:39:18 +0900

    URL내에서 변수 사용 방법 학습

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f"User {username}"
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
