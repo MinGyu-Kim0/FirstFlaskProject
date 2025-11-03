3d4f63d12375865bcf62f298c6a7d4b3c92c5041
MINGYU KIM <coinmagic8085@gmail.com>
2025-11-03 21:35:47 +0900

    Flask의 HTTP Method 학습

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return f"User {username}"
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return "Logging in..."
+    else:
+        return "Login Form"
 
 if __name__ == '__main__':
     app.run(debug=True)
